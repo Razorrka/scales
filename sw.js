1dbb82fd86fd5894e3f29e9d607c0362b181427e
Razorrka <Razorrka@users.noreply.github.com>
2026-07-17 23:36:58 -0400

    Theme orbs: pomegranate, peach glow, tokyo - glowing ombre themes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'scales-v1';
+const CACHE = 'scales-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
                 './apple-touch-icon.png', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
