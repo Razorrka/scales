f54f048dfdcba5ce5987c8b7c6aacd02144c96a4
Razorrka <Razorrka@users.noreply.github.com>
2026-08-15 23:15:05 -0400

    Make the view switcher a labelled 44px button so the tabs are findable

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'scales-202608152228';
+const CACHE = 'scales-202608152315';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
                 './apple-touch-icon.png', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
